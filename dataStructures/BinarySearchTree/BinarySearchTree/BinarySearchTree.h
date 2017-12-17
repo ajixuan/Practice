0449818e555cdbd012af1e3f45d25d46f220bfc7
Allen Ji <xuan.ji90@gmail.com>
2017-12-17 17:08:54 -0500

    Array BST complete

--- dataStructures/BinarySearchTree/BinarySearchTree/BinarySearchTree.h
+++ dataStructures/BinarySearchTree/BinarySearchTree/BinarySearchTree.h
@@ -1,10 +1,14 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <cmath>
 
 class BST {
 	std::vector<int> tree;
 public:
+	//Initialize a 10 level tree
+	BST() { this->tree = std::vector<int>((int)std::pow(2, 10), NULL); }
+	void trickle(int node, int root);
 	void insert(int);
 	void print_tree();
 };
