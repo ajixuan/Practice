397239334c015401ca9e7462600b82160a506122
Allen Ji <xuan.ji90@gmail.com>
2018-02-11 18:55:11 -0500

    Coinsum reimplementation, still failing test

--- Java/DP/CoinSum/src/main/java/CoinSum.java
+++ Java/DP/CoinSum/src/main/java/CoinSum.java
@@ -1,3 +1,5 @@
+import java.util.function.Function;
+
 /**
  * Problem:
  *  Given a list of coins of different values, what is the fewest number of coins needed to
@@ -13,61 +15,79 @@
  *
  */
 public class CoinSum {
-    private int[] coins;
-    private int[] solutions;
-    private int sum;
 
-    public CoinSum(int[] coins, int sum){
-       this.coins = coins;
-       this.sum = sum;
-       this.solutions = new int[sum+1];
-    }
+    /**
+     * Find the remainder on a coin after optimal
+     * @param coins
+     * @param sum
+     * @return
+     */
+    public int findRemainder(int[] coins, int sum){
+        while (sum != 0) {
+            int tok = 0;
 
+            //Choose the largest possible coin
+            tok = findLargestCoin(coins,sum);
 
-    public void setCoins(int[] coins){
-        this.coins = coins;
-    }
-
-    public void setSum(int sum){
-        this.sum = sum;
-        this.solutions = new int[sum+1];
-    }
-
-    public int getCoinSum(){
-        //Main loop
-        for(int n = 0; n <= sum; n++){
-            if(n == 0){
-                this.solutions[n] = 0;
-                continue;
-            }
-
-            //Get previous solutions to reduce problem size
-            this.solutions[n] = Math.min(this.solutions[n-1] + this.findMinCoins(1), this.findMinCoins(n));
+            if(tok == -1) return sum;
+            sum -= tok;
         }
-
-        return this.solutions[sum];
+        return sum;
     }
 
     /**
      * Find the biggest possible coin for the given sum
+     *
      * @param sum
      * @return
      */
-    public int findMinCoins(int sum){
+    public int findMinCoins(int[] coins,int sum) {
         int ans = 0;
-        int tok = 0;
-        while(sum != 0){
-            //Choose the largest coin
-            for(int coin : this.coins){
-                if(coin <= sum){
-                    tok = coin;
-                }
-            }
-            sum-=tok;
+
+        //If no coin is small enough for sum
+        if(findLargestCoin(coins, sum) == -1) return -1;
+
+        while (sum != 0) {
+            //Choose the largest possible coin
+            int coin = findLargestCoin(coins, sum);
+
+            if(coin == -1) return ans;
+            sum -= coin;
             ans++;
         }
 
         return ans;
     }
 
+    private int findLargestCoin(int[] coins, int amount) {
+        int tok = -1;
+        for (int coin : coins) {
+            if (coin <= amount && coin > tok) tok = coin;
+        }
+        return tok;
+    }
+
+
+    public int coinChange(int[] coins, int amount) {
+        int[] solutions = new int[amount+1];
+        if(amount == 0) return 0;
+        int remainder = 0, reCompute, aggregateSol;
+
+        for (int n = 0; n <= amount; n++) {
+            if (n == 0) {
+                solutions[n] = 0;
+                continue;
+            }
+
+            //If there is remainder
+            remainder = findRemainder(coins, n);
+            reCompute = findMinCoins(coins, n);
+            solutions[n] = reCompute;
+
+            if(remainder != 0 && solutions[n-1] < 0){
+                   solutions[n] = Math.min(reCompute, solutions[n-1] + findMinCoins(coins, remainder));
+            }
+        }
+        return (remainder > 0) ? -1 : solutions[amount];
+    }
 }
--- Java/DP/CoinSum/src/test/java/TestCoinSum.java
+++ Java/DP/CoinSum/src/test/java/TestCoinSum.java
@@ -1,29 +1,45 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestCoinSum {
 
-
-    @Test
-    public void testOne(){
-        CoinSum sum = new CoinSum(new int[]{1,3,5}, 11);
-        Assert.assertEquals(3, sum.getCoinSum());
-        sum.setSum(12);
-        Assert.assertEquals(4, sum.getCoinSum());
-        sum.setSum(13);
-        Assert.assertEquals(3, sum.getCoinSum());
-        sum.setSum(15);
-        Assert.assertEquals(3, sum.getCoinSum());
+    CoinSum sum;
 
 
+    @Before
+    public void setup(){
+        this.sum = new CoinSum();
     }
 
     @Test
+    public void testOne(){
+        Assert.assertEquals(3, sum.coinChange(new int[]{1,3,5}, 11));
+        Assert.assertEquals(4, sum.coinChange(new int[]{1,3,5}, 12));
+        Assert.assertEquals(3, sum.coinChange(new int[]{1,3,5}, 13));
+        Assert.assertEquals(3, sum.coinChange(new int[]{1,3,5}, 15));
+    }
+
+
+    @Test
     public void testMinCoin(){
-        CoinSum sum = new CoinSum(new int[]{1,3,5}, 11);
-        Assert.assertEquals(1, sum.findMinCoins(3));
-        Assert.assertEquals(2, sum.findMinCoins(2));
-        Assert.assertEquals(2, sum.findMinCoins(6));
-        Assert.assertEquals(3, sum.findMinCoins(9));
+        CoinSum sum = new CoinSum();
+        Assert.assertEquals(1, sum.findMinCoins(new int[]{1,3,5}, 1));
+        Assert.assertEquals(2, sum.findMinCoins(new int[]{1,3,5}, 2));
+        Assert.assertEquals(2, sum.findMinCoins(new int[]{1,3,5}, 6));
+        Assert.assertEquals(3, sum.findMinCoins(new int[]{1,3,5}, 9));
+    }
+
+    @Test
+    public void testEdges() {
+        Assert.assertEquals(-1, sum.coinChange(new int[]{2}, 1));
+        Assert.assertEquals(-1, sum.coinChange(new int[]{2}, 3));
+        Assert.assertEquals(2, sum.coinChange(new int[]{2}, 4));
+    }
+
+    @Test
+    public void testHard() {
+        //Assert.assertEquals(4, sum.coinChange(new int[]{2,5,10,1}, 27));
+        Assert.assertEquals(20, sum.coinChange(new int[]{186,419,83,408}, 6249));
     }
 }
