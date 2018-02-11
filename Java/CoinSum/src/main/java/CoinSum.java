1065b8f3715d49f4c3adac5c04b1c7907062a1fd
Allen Ji <xuan.ji90@gmail.com>
2018-02-10 22:14:37 -0500

    CoinSum problem for dynamic programming

---
+++ Java/CoinSum/src/main/java/CoinSum.java
@@ -0,0 +1,63 @@
+/**
+ * Problem:
+ *  Given a list of coins of different values, what is the fewest number of coins needed to
+ *  make up that amount?
+ *
+ * Dynamic Programming:
+ *  - The solution is built up from computations of smaller problems
+ *  - Grow from smaller problems to bigger problems
+ *
+ * Deal with subproblems first
+ * how to sum 1 coin -> n coins
+ * Each step is an array listing the minimum
+ *
+ */
+public class CoinSum {
+    private int[] coins;
+    private int[] solutions;
+    private int sum;
+
+    public CoinSum(int[] coins, int sum){
+       this.coins = coins;
+       this.sum = sum;
+       this.solutions = new int[sum+1];
+    }
+
+    public int getCoinSum(){
+        //Main loop
+        for(int n = 0; n <= sum; n++){
+            if(n == 0){
+                this.solutions[n] = 0;
+                continue;
+            }
+
+            //Get previous solutions to reduce problem size
+            this.solutions[n] = Math.min(this.solutions[n-1] + this.findMinCoins(1), this.findMinCoins(n));
+        }
+
+        return this.solutions[sum];
+    }
+
+    /**
+     * Find the biggest possible coin for the given sum
+     * @param sum
+     * @return
+     */
+    public int findMinCoins(int sum){
+        int ans = 0;
+        int tok = 0;
+        while(sum != 0){
+            //Choose the largest coin
+            for(int coin : this.coins){
+                if(coin <= sum){
+                    tok = coin;
+                }
+            }
+            sum-=tok;
+            ans++;
+        }
+
+        return ans;
+    }
+
+}
---
+++ Java/CoinSum/src/test/java/TestCoinSum.java
@@ -0,0 +1,26 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestCoinSum {
+
+
+    @Test
+    public void testOne(){
+        CoinSum sum = new CoinSum(new int[]{1,3,5}, 11);
+        Assert.assertEquals(3, sum.getCoinSum());
+        Assert.assertEquals(3, sum.getCoinSum());
+        Assert.assertEquals(3, sum.getCoinSum());
+        Assert.assertEquals(3, sum.getCoinSum());
+
+
+    }
+
+    @Test
+    public void testMinCoin(){
+        CoinSum sum = new CoinSum(new int[]{1,3,5}, 11);
+        Assert.assertEquals(1, sum.findMinCoins(3));
+        Assert.assertEquals(2, sum.findMinCoins(2));
+        Assert.assertEquals(2, sum.findMinCoins(6));
+        Assert.assertEquals(3, sum.findMinCoins(9));
+    }
+}
