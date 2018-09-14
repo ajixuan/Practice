317ce3d5b48e67891f74412a700a03313bd43f15
ajixuan <xuan.ji90@gmail.com>
2018-09-13 23:48:47 -0400

    Created proper random list

--- Java/FindNDuplicate/src/main/java/FindNDuplicate.java
+++ Java/FindNDuplicate/src/main/java/FindNDuplicate.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class FindNDuplicate {
@@ -7,36 +7,52 @@ public class FindNDuplicate {
 
     /**
      * FindNDuplicate
-     *   adding extra duplicates results in 1 + moreDupes number of duplicates
+     * adding extra duplicates results in 1 + moreDupes number of duplicates
+     *
      * @param n
      * @param moreDupes
      */
     public FindNDuplicate(int n, int moreDupes) {
         if (moreDupes < 0) {
             throw new RuntimeException("Duplicates must be >= 0");
+        } else if (moreDupes > n / 2) {
+            throw new RuntimeException("Duplicates cannot be > n/2");
         }
 
         this.list = FindNDuplicate.makeList(n);
         Random r = new Random();
-        IntStream distinct = r.ints(n * 4, 0, n).distinct();
-        IntStream indexes = r.ints(0, n).distinct();
+
+        int bound = (n > 2)? n - 2 : 1;
+
+        int[] randoms = r.ints(0, n).distinct().limit(moreDupes).sorted().toArray();
+        Set<Integer> taken = new HashSet<Integer>();
+
+        int index = r.nextInt(bound);
         while (moreDupes > 0) {
-            list[indexes.findAny().getAsInt()] = distinct.findAny().getAsInt();
+            while (taken.contains(index)) {
+                index = r.nextInt(bound);
+            }
+            index = (index % 2 == 1) ? index + 1 : index;
+            this.list[index] = randoms[moreDupes - 1];
+            this.list[index + 1] = randoms[moreDupes - 1];
             moreDupes--;
+            taken.add(index);
         }
+
     }
 
     /**
      * FindNDuplicate
-     *   Creates a list that will have only 1 duplicate
+     * Creates a list that will have only 1 duplicate
+     *
      * @param n
      */
-    public FindNDuplicate(int n){
+    public FindNDuplicate(int n) {
         this.list = FindNDuplicate.makeList(n);
     }
-    
-    
-    private static int[] makeList(int n){
+
+
+    private static int[] makeList(int n) {
         int[] list = new int[n + 1];
         for (int i = 0; i < n; i++) {
             list[i] = i;
@@ -47,10 +63,30 @@ public class FindNDuplicate {
         list[n] = list[indexes.findAny().getAsInt()];
         return list;
     }
-    
-    
-    public int[] getList(){
+
+    public int[] getList() {
         return this.list;
+    }
+
+    /**
+     * findDupes()
+     * Finds all the duplicates in the list and returns them
+     *
+     * @return A list of duplicates
+     */
+    public List findDupes() {
+        //Divide and conquer
+        System.out.println(list.toString());
+        return null;
+
+    }
+
+    public void printList() {
+        for (int i : this.list) {
+            System.out.printf("%d,", i);
+        }
+        System.out.print("\n");
+
     }
 
 }
--- Java/FindNDuplicate/src/test/java/TestFindNDuplicate.java
+++ Java/FindNDuplicate/src/test/java/TestFindNDuplicate.java
@@ -6,8 +6,23 @@ public class TestFindNDuplicate {
     @Test
     public void testMakeList() {
         FindNDuplicate nlist = new FindNDuplicate(1, 0);
+        nlist.printList();
         Assert.assertEquals(0, nlist.getList()[0]);
         Assert.assertEquals(0, nlist.getList()[1]);
+
+        nlist = new FindNDuplicate(2, 0);
+        nlist.printList();
+        Assert.assertEquals(0, nlist.getList()[0]);
+        Assert.assertEquals(1, nlist.getList()[1]);
+        Assert.assertEquals(3, nlist.getList().length);
+
+        nlist = new FindNDuplicate(10, 5);
+        nlist.printList();
+        Assert.assertEquals(11, nlist.getList().length);
+    }
+
+    @Test
+    public void testFindDupe(){
 
     }
 }
